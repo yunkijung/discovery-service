24d215c656ba99b867ff464492725cbc59d5b6ac
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2023-12-05 00:23:23 -0500

    init

---
+++ src/test/java/com/loglrs/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loglrs.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
